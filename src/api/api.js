ec0f781cd719612ce485b066d8c8c4e39d21b51b
Aleksandr <aleksandera9@gmail.com>
2023-01-17 21:43:05 +0300

    67: thunk auth-reducer and profile-reducer

--- src/api/api.js
+++ src/api/api.js
@@ -25,3 +25,9 @@ export const usersAPI = {
 		return instance.get(`profile/` + userId);
 	}
 }
+
+export const authAPI = {
+	me() {
+		return instance.get(`auth/me`)
+	}
+}
--- src/components/Dialogs/DialogsContainer.jsx
+++ src/components/Dialogs/DialogsContainer.jsx
@@ -1,4 +1,5 @@
 
+// eslint-disable-next-line no-unused-vars
 import React from 'react';
 import { sendMessageCreator, updateNewMessageBodyCreator } from "../../redux/dialogs-reducer";
 import Dialogs from "./Dialogs";
