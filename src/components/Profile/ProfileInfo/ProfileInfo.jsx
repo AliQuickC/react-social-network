0501626c0c69daddfab41e31477175970bf64738
Aleksandr <aleksandera9@gmail.com>
2023-01-24 11:53:51 +0300

    71: local state

--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import s from './ProfileInfo.module.css';
 import Preloader from "../../common/Preloader/Preloader";
 import userPhoto from "../../../assets/images/user.png";
+import ProfileStatus from "./ProfileStatus"
 
 const ProfileInfo = (props) => {
 	if (!props.profile) {
@@ -9,19 +10,16 @@ const ProfileInfo = (props) => {
 	}
 	return (
 		<div>
-			<div>
-				<img
-					src='https://pro-goa.ru/wp-content/uploads/luchshie-plyazhi-goa-03.jpg' alt="profile" />
-			</div>
 			<div className={s.descriptionBlock}>
 				<img	className={s.profileImage}
 							src={props.profile.photos.large != null ? props.profile.photos.large : userPhoto}
 							alt="avatar" />
-							<h4 className={s.profileTitle}>Имя: {props.profile.fullName}</h4>
-							<p>Про меня: {props.profile.aboutMe}</p>
-							<p>Контакты: {JSON.stringify(props.profile.contacts)}</p>
-							<p>Ищу работу: {props.profile.lookingForAJob ? 'Да' : 'Нет'}</p>
-							<p>Какую вакансию ищу: {props.profile.lookingForAJobDescription ? props.profile.lookingForAJobDescription : ''}</p>
+				<ProfileStatus status={"Hello my friends"}/>
+				<h4 className={s.profileTitle}>Имя: {props.profile.fullName}</h4>
+				<p>Про меня: {props.profile.aboutMe}</p>
+				<p>Контакты: {JSON.stringify(props.profile.contacts)}</p>
+				<p>Ищу работу: {props.profile.lookingForAJob ? 'Да' : 'Нет'}</p>
+				<p>Какую вакансию ищу: {props.profile.lookingForAJobDescription ? props.profile.lookingForAJobDescription : ''}</p>
 			</div>
 		</div>
 	)
