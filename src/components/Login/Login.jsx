7b11cb1e19ee88569e6e8f53e3d71c06538a8e77
Aleksandr <aleksandera9@gmail.com>
2023-02-04 12:56:13 +0300

    77: redux-form field validation

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Field, reduxForm } from "redux-form";
+import {Input} from "../common/FormsControls/FormsControls";
+import {required} from "../../utils/validators/validators";
 
 const LoginForm = (props) => {
 	// Field - компонент из библиотеки redux-form
@@ -7,13 +9,19 @@ const LoginForm = (props) => {
 	return (
 		<form onSubmit={props.handleSubmit}>
 			<div>
-				<Field placeholder={"Login"} name={"login"} component={"input"} />
+				<Field	name={"login"}
+								placeholder={"Login"}
+								component={Input}
+								validate={[required]} />
 			</div>
 			<div>
-				<Field placeholder={"Password"} name={"password"} component={"input"} />
+				<Field	name={"password"}
+								placeholder={"Password"}
+								component={Input}
+								validate={[required]}	/>
 			</div>
 			<div>
-				<Field component={"input"} name={"rememberMe"} type={"checkbox"} /> remember me
+				<Field component={Input} name={"rememberMe"} type={"checkbox"} /> remember me
 			</div>
 			<div>
 				<button>Login</button>
