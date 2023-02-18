7fead9f4f66006a6f5e35c30882d62591d074a25
Aleksandr <aleksandera9@gmail.com>
2023-02-18 18:50:36 +0300

    Updates

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import './index.css';
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import JSApp from './App';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<JSApp/>);
